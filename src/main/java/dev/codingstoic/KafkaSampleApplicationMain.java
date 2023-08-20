ebba7654b8c61e75d49126ef4e61e30d0181099b
Michael Suleiman <mosuleiman19@gmail.com>
2023-08-20 14:17:31 +0400

    Basic configuration

---
+++ src/main/java/dev/codingstoic/KafkaSampleApplicationMain.java
@@ -0,0 +1,11 @@
+package dev.codingstoic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KafkaSampleApplicationMain {
+    private static final Logger logger = LoggerFactory.getLogger(KafkaSampleApplicationMain.class);
+    public static void main(String[] args) {
+        logger.info("Welcome to a Kafka sample project...!");
+    }
+}
